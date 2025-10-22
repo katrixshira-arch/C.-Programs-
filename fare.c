116570c866c5744c0d68da4dbb7efb3cc0b7befc
katrixshira-arch <katrixshira@gmail.com>
2025-10-22 11:37:05 +0300

    Create fare.c

---
+++ fare.c
@@ -0,0 +1,28 @@
+
+/*
+NAME:SHIPHRAH MIROYA 
+REG : PA106/G/28756/25
+DESCRIPTION : A PROGRAM THAT CALCULATES THE AMOUNT OF FARE
+A PASSENGER IS TO BE CHARGED BY MULTIPLYING THE KILOMETRES TRAVELED BY 50
+*/
+
+#include<stdio.h>
+int main()
+{
+//declaring the variables
+int fare;
+int kilometers;
+
+//prompts the user to input the distance they traveled
+printf("Enter the distance to be travelled in kilometres :");
+scanf("%d",&kilometers); 
+
+//main calculation to obtain fare
+fare = kilometers* 50 ;
+
+
+//displays the fare to the user
+printf("\nYOUR FARE IS KSH:");
+printf("%d",fare);
+
+return 0;
